9bbaa4ed27a226ac8f807a0aeea44d834a212914
An Ninh <ninhza2005@gmail.com>
2025-11-20 17:58:58 +0700

    Add project files.

---
+++ 2380601602_NguyenDinhAnNinh_BTVN-B1/PhuongTrinhBac1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace B1_2380601602_NguyenDinhAnNinh_BTVN
+{
+    class PhuongTrinhBac1
+    {
+        protected int a { get;set;}
+        protected int b { get;set;}
+
+        public PhuongTrinhBac1(int a, int b)
+        {
+            this.a = a;
+            this.b = b;
+        }
+
+        public double Calc ()
+        {
+            if (a == 0) return 0;
+            return -b / (double)a;
+        }
+        
+    }
+
+}
